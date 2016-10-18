155066a362e055294a505c2dbaa2b8c5e7971378
Peter Banik <peter@prioritylane.com>
2016-10-18 23:43:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var winston = require('winston');
+var expressWinston = require('express-winston');
+
+
+module.exports = function () {
+	var colorize = process.env.NODE_ENV === 'production' ? false : true;
+
+	// Logger to capture all requests and output them to the console.
+	// [START requests]
+	var requestLogger = expressWinston.logger({
+		transports: [
+			new winston.transports.Console({
+				json: false,
+				colorize: colorize
+			})
+		],
+		expressFormat: true,
+		meta: false
+	});
+	// [END requests]
+
+	// Logger to capture any top-level errors and output json diagnostic info.
+	// [START errors]
+	var errorLogger = expressWinston.errorLogger({
+		transports: [
+			new winston.transports.Console({
+				json: true,
+				colorize: colorize
+			}),
+		]
+	});
+	// [END errors]
+
+	return {
+		requestLogger: requestLogger,
+		errorLogger: errorLogger,
+		error: winston.error,
+		warn: winston.warn,
+		info: winston.info,
+		log: winston.log,
+		verbose: winston.verbose,
+		debug: winston.debug,
+		silly: winston.silly
+	};
+
+};
